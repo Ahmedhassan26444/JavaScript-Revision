bad12d279e4d5de1780291bcf7a9c53ba2e09dac
Ahmad <ahmedhassan.26444@gmail.com>
2025-04-19 12:58:20 -0800

    completed

---
+++ JavaScript Introduction 1/index.js
@@ -0,0 +1,17 @@
+alert("hello my name is ahmad");
+
+let x = 40;
+console.log(typeof x);
+
+let y = 33;
+let z = true;
+console.log(y + 12 + z); // why 46 beacuse of true dontes one and fasle dontes zero
+
+let a = "33";
+let b = 12;
+console.log(a + b); // why 3312 not 45 becuse 33 become string now not a number
+
+let name = "ahmad"
+let lastname = "hassan"
+console.log( "hy my name is :- " + name  + " " + lastname + "."  );  // concatination 
+console.log( `hy my name is :-  ${name}  ${lastname}.`); 
