05815e092ff15a02d37c90d41e084edb3cc16afa
Ahmad <ahmedhassan.26444@gmail.com>
2025-04-20 10:38:34 -0800

    completed

---
+++ arrays-objects 4/index.js
@@ -0,0 +1,16 @@
+// arrays 
+let arr = [11,22,33,44,44];
+console.log([arr]); 
+
+let arr2 = new Array(6);
+console.log(arr2);
+// methods
+let arr3 = [11,22,33,44,44];
+arr3.push(88);
+arr3.shift();
+arr3.unshift(77);
+arr3.splice(3, 2);
+arr3.reverse();
+console.log(arr3); 
+
+
