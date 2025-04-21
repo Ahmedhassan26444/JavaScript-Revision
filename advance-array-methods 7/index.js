7c3c4240ddf40c509df8de741063daad4f40f124
Ahmad <ahmedhassan.26444@gmail.com>
2025-04-21 07:52:57 -0800

    completed

---
+++ advance-array-methods 7/index.js
@@ -0,0 +1,22 @@
+// arrow function 
+const myfunction = (a,b) =>{
+         console.log(a+b);
+}
+myfunction(5, 7);
+
+
+ const arr = [22 , 44 , 55 , 77];
+const function12 = (value,index) =>{
+    if(value === 55){
+        console.log("number exist");
+    }
+}
+arr.find(function12);
+
+
+const array = [1, 2, 3, 4];
+array.forEach(value => {
+    console.log(value);
+});
+
+
